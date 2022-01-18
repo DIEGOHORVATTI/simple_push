99051186ab73ecbf131090e284d5c3225c4df3d0
DIEGOHORVATTI <susanomonkey@gmail.com>
2022-01-18 18:18:17 -0400

    "ajuste"

--- push_git.sh
+++ push_git.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 read -p 'commit: ' commit
-echo $commit
-echo "\"$commit\""
+
 git branch -M main &&
 git add . &&
 git commit -m '"'$commit'"' &&
@@ -10,4 +9,3 @@ git push -u origin main &&
 
 echo -e "\033[0;32m.............................\n\033]"
 echo -e "\033[0;32m| commit: '"'$commit'"'  \n\033]"
-echo -e "\033[0;32m.............................\n\033]\n"
