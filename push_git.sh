351f1daf95d77991f00829e92f7c3384ab6fdeb4
DIEGOHORVATTI <susanomonkey@gmail.com>
2022-01-18 18:44:01 -0400

    teste_3

--- push_git.sh
+++ push_git.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 read -p 'commit: ' commit
+read -p 'branch: ' branch
 
-git branch -M main &&
+git branch -M $branch &&
 git add . &&
 git commit -m ''$commit'' &&
-git push -u origin main &&
+git push -u origin $branch &&
 
 echo -e "\033[0;32m.............................\n\033]"
 echo -e "\033[0;32m           |SUCCESS| \n\033]"
 echo -e "\033[0;32m.............................\n\033]"
 echo -e "\033[0;32m| commit: $commit  \n\033]"
+echo -e "\033[0;32m| branch: $branch  \n\033]"
 echo -e "\033[0;32m.............................\n\033]"
