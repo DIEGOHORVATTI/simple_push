53d9897511f6871d306d2dfabd73add00f0db766
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-08-28 20:37:59 -0400

    fix: atualização

--- push_git.sh
+++ push_git.sh
@@ -1,58 +1,9 @@
 #!/bin/bash
 
-### Função que mostra a versão atual do programa
-versao() {
-echo "v 1.0"
-exit
-}
-
-### Função para mostras as opções de uso do programa
-help(){
-echo "Uso: $(basename "$0") [OPÇÔES]
-
-OPÇÕES
-  -p, --progressive	Inicia o cronometro em ordem progressiva
-			Obs.:Para limitar o tempo na contagem progressiva,
-			é necessário informar o tempo final no formato
-			hh:mm:ss.
-
-  -r, --regressive	Inicia o cronometro em ordem regressiva.
-			Obs.: Necessário informar tempo inicial 
-			no formato hh:mm:ss.
-
-  -h, --help		Mostra esta tela de ajuda e sai
-
-  -v, --version		Mostra a versão do programa e sai
-
-EXEMPLOS DE USO: 
-   $./pomodoro -p.................contagem progessiva infinita
-   $./pomodoro -p 01:00:00........contagem progressiva de 1 hora
-   $./pomodoro -r 01:00:00........contagem regressiva de 1 hora
-" && exit 1
-}
-
-
-read -p 'commit: ' commit
-read -p 'branch: ' branch
-
-git branch -M $branch &&
-git add . &&
-git commit -m ''$commit'' &&
-git push -u origin $branch &&
-
-echo -e "\033[0;32m.............................\n\033]"
-echo -e "\033[0;32m           |SUCCESS| \n\033]"
-echo -e "\033[0;32m.............................\n\033]"
-echo -e "\033[0;32m| commit: $commit  \n\033]"
-echo -e "\033[0;32m| branch: $branch  \n\033]"
-echo -e "\033[0;32m.............................\n\033]"
-
-while test -n "$1"
-do
-    case "$1" in
-        -h | --help) help ;;
-        -h | --help) help ;;
-       *) help ;;
-    esac
-    shift
-done
+read -p ' New commit: ' git
+git branch -M main && 
+git add . && 
+git commit -m "$git" && 
+git push -u origin main &&
+clear &&
+echo -e "\n\033[0;34m ✓✓:\033[0m $git \n"
